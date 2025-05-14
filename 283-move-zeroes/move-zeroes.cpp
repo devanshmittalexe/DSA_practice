14545c0285a3058b7d2af614538736bb939870b7
Devansh Mittal <109277855+devanshmittalexe@users.noreply.github.com>
2025-05-14 16:10:25 +0530

    Time: 0 ms (100.00%) | Memory: 24.4 MB (8.57%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -2,7 +2,7 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int p1=-1,p2=0,n=nums.size();
+        int n=nums.size();
         queue<int>a;
         for(int i=0;i<n;i++){
             if(nums[i]==0) a.push(i);
