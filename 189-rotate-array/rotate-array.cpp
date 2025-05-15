4b4f51a2f25300e696cf99d58f9b3a35f2c10bc8
Devansh Mittal <109277855+devanshmittalexe@users.noreply.github.com>
2025-05-15 07:46:37 +0530

    Time: 0 ms (100.00%) | Memory: 29.5 MB (78.65%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,14 +1,19 @@
-// brute force
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+        reverse(nums.begin(),nums.end());
         int n=nums.size();
         k=k%n;
-        if(k<n){
-            vector<int>a;
-            for(int i=n-k;i<n;i++) a.push_back(nums[i]);
-            for(int i=0;i<n-k;i++) a.push_back(nums[i]);
-            nums=a;
+        int p1=0,p2=k-1,p3=k,p4=n-1;
+        while(p1<p2){
+            swap(nums[p1],nums[p2]);
+            p1++;
+            p2--;
+        }
+        while(p3<p4){
+            swap(nums[p3],nums[p4]);
+            p3++;
+            p4--;
         }
     }
 };
