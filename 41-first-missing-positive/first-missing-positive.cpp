ea1bbbd557f55cb8ae4f5c0a41b22e6fa622cf27
Devansh Mittal <109277855+devanshmittalexe@users.noreply.github.com>
2025-05-18 00:59:44 +0530

    Time: 0 ms (100.00%) | Memory: 54.9 MB (93.42%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -10,8 +10,9 @@ public:
             int &z=nums[i];
             if(z<1 || z>n) z=n+1;
         }
+        int temp;
         for(int i=0;i<n;i++){
-            int temp=abs(nums[i]);
+            temp=abs(nums[i]);
             if((temp-1<n) && nums[temp-1]>0) nums[temp-1]*=-1;
         }
         for(int i=0;i<n;i++) if(nums[i]>0) return i+1;
