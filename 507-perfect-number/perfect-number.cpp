10fa7da5bc023109e3f3dde13e01edf0b3fbfd66
Devansh Mittal <109277855+devanshmittalexe@users.noreply.github.com>
2025-05-19 14:48:45 +0530

    Time: 1042 ms (46.77%) | Memory: 7.7 MB (87.37%) - LeetSync

--- 507-perfect-number/perfect-number.cpp
+++ 507-perfect-number/perfect-number.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     bool checkPerfectNumber(int num) {
         int s=0;
-        for(int i=1;i<=num/2;i++){
+        int temp=num/2;
+        for(int i=1;i<=temp;i++){
             if(num%i==0) s+=i;
         }
         return s==num;
