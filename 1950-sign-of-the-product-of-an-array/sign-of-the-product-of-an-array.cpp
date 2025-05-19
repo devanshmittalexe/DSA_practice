e148945d7d2b38a0dd8dedf31eedf2c7e1c2ea63
Devansh Mittal <109277855+devanshmittalexe@users.noreply.github.com>
2025-05-19 17:47:24 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (48.30%) - LeetSync

---
+++ 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int nc=0;
+        for(auto &i:nums){
+            if(i==0) return 0;
+            if(i<0) nc++;
+        }
+        return (nc&1)?-1:1;
+    }
+};
