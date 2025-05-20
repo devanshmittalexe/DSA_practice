9d9e85d236495826fa01a7f43579d4db3fcd8f77
Devansh Mittal <109277855+devanshmittalexe@users.noreply.github.com>
2025-05-20 12:15:12 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (35.01%) - LeetSync

--- 319-bulb-switcher/bulb-switcher.cpp
+++ 319-bulb-switcher/bulb-switcher.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     int bulbSwitch(int n) {
         int bc=0,t;
-        for(int i=1;i<=n;i++){
-            t=sqrt(i);
-            if(t*t==i) bc++;
-        }
+        // for(int i=1;i<=n;i++){
+        //     t=sqrt(i);
+        //     if(t*t==i) bc++;
+        // }
+        for(int i=1;i*i<=n;i++) bc++;
         return bc;
     }
 };
