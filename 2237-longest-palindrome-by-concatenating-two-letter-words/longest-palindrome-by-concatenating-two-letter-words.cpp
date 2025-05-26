74220169153aaf118a05c496ae35b3bd108ba36b
Devansh Mittal <109277855+devanshmittalexe@users.noreply.github.com>
2025-05-26 10:58:39 +0530

    Time: 117 ms (38.69%) | Memory: 171.9 MB (43.85%) - LeetSync

--- 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -1,3 +1,24 @@
+class Solution {
+public:
+    int longestPalindrome(vector<string>& words) {
+        unordered_map<string,int>mp;
+        string g;
+        int ans=0;
+        for(auto &i:words){
+            g=i;
+            swap(g[0],g[1]);
+            if(mp.count(g) && mp[g]>0){
+                mp[g]--;
+                ans+=4;
+            }
+            else mp[i]++;
+        }
+        for(auto &[i,j]:mp){
+            if(i[0]==i[1] && j>0) return ans+2;
+        }
+        return ans;
+    }
+};
 // class Solution {
 // public:
 //     int longestPalindrome(vector<string>& words) {
@@ -8,42 +29,20 @@
 //         for(auto &i:words){
 //             string g=i;
 //             swap(g[0],g[1]);
-//             if(g[0]==g[1]){
-//                 m[g]++;
-//                 if(m[g]>ma) ma=m[g];
-//             }
 //             if(s.count(g)){
 //                 ans+=4;
 //                 s.erase(g);
 //             }
+//             // if(g[0]==g[1]){
+//             //     m[g]++;
+//             //     if(m[g]>ma) ma=m[g];
+//             // }
 //             else s.insert(i);
 //         }
-//         return ans+(ma*2);
+//         for(auto &i:s){
+//             if(i[0]==i[1]) return ans+2;
+//         }
+//         return ans;
 //     }
 // };
-class Solution {
- public:
-  int longestPalindrome(vector<string>& words) {
-    int ans = 0;
-    vector<vector<int>> count(26, vector<int>(26));
-
-    for (const string& word : words) {
-      const int i = word[0] - 'a';
-      const int j = word[1] - 'a';
-      if (count[j][i]) {
-        ans += 4;
-        --count[j][i];
-      } else {
-        ++count[i][j];
-      }
-    }
-
-    for (int i = 0; i < 26; ++i)
-      if (count[i][i])
-        return ans + 2;
-
-    return ans;
-  }
-};
-
 
