51fc1d7bf9bbe872d01a8d6b4f43ce68c165a90f
Devansh Mittal <109277855+devanshmittalexe@users.noreply.github.com>
2025-06-01 18:10:13 +0530

    Time: 23 ms (62.94%) | Memory: 9.1 MB (5.29%) - LeetSync

---
+++ 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long distributeCandies(int n, int limit) {
+        long long ans=0;
+        int minch1=max(0,n-2*limit);
+        int maxch1=min(limit,n);
+        for(int i=minch1;i<=maxch1;i++){
+            //for child 2;
+            int N=n-i;
+            int minch2=max(0,N-limit);
+            int maxch2=min(N,limit);
+            ans+=maxch2-minch2+1;
+        }
+        return ans;
+
+    }
+};
