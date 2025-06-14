907e0d4770ed2621c3f5f1b1d77b6de6d78cc862
Devansh Mittal <109277855+devanshmittalexe@users.noreply.github.com>
2025-06-14 22:27:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (84.91%) - LeetSync

---
+++ 2704-maximum-difference-by-remapping-a-digit/maximum-difference-by-remapping-a-digit.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minMaxDifference(int num) {
+        string s=to_string(num);
+        char x,y;
+        for(auto &i:s){
+            if(i!='9'){
+                x=i;
+                break;
+            } 
+        }
+        for(auto &i:s){
+            if(i!='0'){
+                y=i;
+                break;
+            }
+        }
+        string s1="",s2="";
+        for(auto &i:s){
+            if(i==x) s1+='9';
+            else s1+=i;
+            if(i==y) s2+='0';
+            else s2+=i;
+        }
+        return abs(stoi(s1)-stoi(s2));
+    }
+};
