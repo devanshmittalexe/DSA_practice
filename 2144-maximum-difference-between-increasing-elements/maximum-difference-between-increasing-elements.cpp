a605ec937a431ebcf2849c6fb4fb1afe42720198
Devansh Mittal <109277855+devanshmittalexe@users.noreply.github.com>
2025-06-16 18:08:43 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (71.76%) - LeetSync

--- 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.cpp
+++ 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.cpp
@@ -4,7 +4,6 @@ public:
         int temp=nums[0];
         int ans=-1;
         for(int i=1;i<nums.size();i++){
-            cout<<nums[i]<<" "<<temp;
             if(nums[i]>temp) ans=max(ans,(nums[i]-temp));
             if(nums[i]<temp) temp=nums[i];
         }
