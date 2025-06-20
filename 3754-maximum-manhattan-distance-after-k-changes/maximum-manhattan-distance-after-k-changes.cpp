fe7e56c5824aca933cd91e56890ae5139f562c4d
Devansh Mittal <109277855+devanshmittalexe@users.noreply.github.com>
2025-06-20 21:02:25 +0530

    Time: 41 ms (95.76%) | Memory: 38.1 MB (90.61%) - LeetSync

---
+++ 3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxDistance(string S, int k) {
+        int cs,ms=0,e=0,w=0,n=0,s=0;
+        for(int i=0;i<S.length();i++){
+            if(S[i]=='S') s++;
+            else if(S[i]=='E') e++;
+            else if(S[i]=='W') w++;
+            else if(S[i]=='N') n++;
+            cs=abs(s-n)+abs(e-w);
+            cs+=min((i+1-cs),k*2);
+            ms=max(ms,cs);
+        }
+        return ms;
+    }
+};
